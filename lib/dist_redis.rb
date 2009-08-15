ca6a516ba5e17f180919afa2c9349419b97a4195
Chris Wanstrath <chris@ozmm.org>
2009-08-15 00:25:25 -0700

    whitespace

--- lib/dist_redis.rb
+++ lib/dist_redis.rb
@@ -4,9 +4,9 @@ class DistRedis
   attr_reader :ring
   def initialize(opts={})
     hosts = []
-    
+
     db = opts[:db] || nil
-    timeout = opts[:timeout] || nil 
+    timeout = opts[:timeout] || nil
 
     raise Error, "No hosts given" unless opts[:hosts]
 
@@ -15,21 +15,21 @@ class DistRedis
       hosts << Redis.new(:host => host, :port => port, :db => db, :timeout => timeout, :db => db)
     end
 
-    @ring = HashRing.new hosts 
+    @ring = HashRing.new hosts
   end
-  
+
   def node_for_key(key)
     if key =~ /\{(.*)?\}/
       key = $1
     end
     @ring.get_node(key)
   end
-  
+
   def add_server(server)
     server, port = server.split(':')
     @ring.add_node Redis.new(:host => server, :port => port)
   end
-  
+
   def method_missing(sym, *args, &blk)
     if redis = node_for_key(args.first.to_s)
       redis.send sym, *args, &blk
@@ -37,7 +37,7 @@ class DistRedis
       super
     end
   end
-  
+
   def keys(glob)
     keyz = []
     @ring.nodes.each do |red|
@@ -45,33 +45,33 @@ class DistRedis
     end
     keyz
   end
-  
+
   def save
     @ring.nodes.each do |red|
       red.save
     end
   end
-  
+
   def bgsave
     @ring.nodes.each do |red|
       red.bgsave
     end
   end
-  
+
   def quit
     @ring.nodes.each do |red|
       red.quit
     end
   end
-  
+
   def delete_cloud!
     @ring.nodes.each do |red|
       red.keys("*").each do |key|
         red.delete key
-      end  
+      end
     end
   end
-  
+
 end
 
 
@@ -94,21 +94,21 @@ r = DistRedis.new 'localhost:6379', 'localhost:6380', 'localhost:6381', 'localho
   p r['urdad2']
   p r['urmom3']
   p r['urdad3']
-  
+
   r.push_tail 'listor', 'foo1'
   r.push_tail 'listor', 'foo2'
   r.push_tail 'listor', 'foo3'
   r.push_tail 'listor', 'foo4'
   r.push_tail 'listor', 'foo5'
-  
+
   p r.pop_tail('listor')
   p r.pop_tail('listor')
   p r.pop_tail('listor')
   p r.pop_tail('listor')
   p r.pop_tail('listor')
-  
+
   puts "key distribution:"
-  
+
   r.ring.nodes.each do |red|
     p [red.port, red.keys("*")]
   end
