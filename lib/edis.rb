c39b02e6f63138f3d4f4a240594947077d27cb99
Nick Quaranto <nick@quaran.to>
2010-03-17 17:21:01 -0400

    Small edis.rb doc update

--- lib/edis.rb
+++ lib/edis.rb
@@ -1,3 +1,3 @@
-# This file allows for the running of rubygems with a nice
+# This file allows for the running of redis-rb with a nice
 # command line look-and-feel: irb -rubygems -redis foo.rb
 require 'redis'
