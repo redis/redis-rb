436fbae81ce30901d963a3d96f00f1981947333c
Ezra Zygmuntowicz <ez@engineyard.com>
2010-04-02 17:38:28 -0700

    forgot to commint subscribe.rb

---
+++ lib/redis/subscribe.rb
@@ -0,0 +1,14 @@
+class Subscription	
+  def subscribe(&block)
+    if block_given? then @subscribe = block else @subscribe end
+  end
+
+  def unsubscribe(&block)	
+    if block_given? then @unsubscribe = block else @unsubscribe end
+  end
+
+  def message(&block)	
+    if block_given? then @message = block else @message end
+  end
+	
+end
