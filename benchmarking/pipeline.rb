5b49cd0659321a2f6328316ecb1fd254e939f3a3
Michel Martens <michel@soveran.com>
2010-08-06 16:38:55 -0300

    Fix pipeline benchmarks.

--- benchmarking/pipeline.rb
+++ benchmarking/pipeline.rb
@@ -1,44 +1,51 @@
-require 'benchmark'
+require "benchmark"
+
 $:.push File.join(File.dirname(__FILE__), 'lib')
+
 require 'redis'
 
-times = 20000
+ITERATIONS = 10000
 
-@r = Redis.new#(:debug => true)
-@r['foo'] = "The first line we sent to the server is some text"
+@r = Redis.new
 
-Benchmark.bmbm do |x|
-  x.report("set") do
-    20000.times do |i|
-      @r["set#{i}"] = "The first line we sent to the server is some text"; @r["foo#{i}"]
+Benchmark.bmbm do |benchmark|
+  benchmark.report("set") do
+    @r.flushdb
+
+    ITERATIONS.times do |i|
+      @r.set("foo#{i}", "Hello world!")
+      @r.get("foo#{i}")
     end
   end
-  
-  x.report("set (pipelined)") do
-    @r.pipelined do |pipeline|
-      20000.times do |i|
-        pipeline["set_pipelined#{i}"] = "The first line we sent to the server is some text"; @r["foo#{i}"]
+
+  benchmark.report("set (pipelined)") do
+    @r.flushdb
+
+    @r.pipelined do
+      ITERATIONS.times do |i|
+        @r.set("foo#{i}", "Hello world!")
+        @r.get("foo#{i}")
       end
     end
   end
-  
-  x.report("push+trim") do
-    20000.times do |i|
-      @r.push_head "push_trim#{i}", i
-      @r.list_trim "push_trim#{i}", 0, 30
+
+  benchmark.report("lpush+ltrim") do
+    @r.flushdb
+
+    ITERATIONS.times do |i|
+      @r.lpush "lpush#{i}", i
+      @r.ltrim "ltrim#{i}", 0, 30
     end
   end
-  
-  x.report("push+trim (pipelined)") do
-    @r.pipelined do |pipeline|
-      20000.times do |i|
-        pipeline.push_head "push_trim_pipelined#{i}", i
-        pipeline.list_trim "push_trim_pipelined#{i}", 0, 30
+
+  benchmark.report("lpush+ltrim (pipelined)") do
+    @r.flushdb
+
+    @r.pipelined do
+      ITERATIONS.times do |i|
+        @r.lpush "lpush#{i}", i
+        @r.ltrim "ltrim#{i}", 0, 30
       end
     end
   end
-end
-
-@r.keys('*').each do |k|
-  @r.delete k
 end
