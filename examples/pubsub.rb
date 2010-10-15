bdaddcf50a2d9dc850649fc3356a0b038cca78f6
Miles Egan <milesegan@gmail.com>
2010-10-15 12:21:14 +0700

    update pubsub example

--- examples/pubsub.rb
+++ examples/pubsub.rb
@@ -6,14 +6,20 @@ puts "  ./redis-cli publish a hello"
 puts "Finally force the example to exit sending the 'exit' message with"
 puts "  ./redis-cli publish b exit"
 puts ""
+
 r = Redis.new(:timeout => 0)
-r.subscribe(:a,:b) {|msg|
-    if msg[:type] == "subscribe"
-        puts "Subscribed to #{msg[:class]} (#{msg[:data]} subscriptions)"
-    elsif msg[:type] == "message"
-        puts "Got data: #{msg.inspect}"
-        if msg[:data] == "exit"
-            r.unsubscribe
-        end
+r.subscribe(:a,:b) do |s|
+
+  s.subscribe do |channel,msg|
+    puts "Subscribed to #{channel} (#{msg} subscriptions)"
+  end
+
+  s.message do |channel,val|
+    puts "Got data: #{val} on channel #{channel}"
+    if val == "exit"
+      r.unsubscribe
     end
-}
+  end
+
+end
+
