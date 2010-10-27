23ed5974e96e89f4ef691139a9380a9d08e9cec6
Damian Janowski & Michel Martens <damian.janowski+soveran@gmail.com>
2010-10-27 00:40:58 -0300

    Add Redis.current as a standard way to grab a connection to Redis.

--- test/internals_test.rb
+++ test/internals_test.rb
@@ -16,3 +16,12 @@ $TEST_INSPECT    = true
 
 load File.expand_path("./lint/internals.rb", File.dirname(__FILE__))
 
+test "Redis.current" do
+  Redis.current.set("foo", "bar")
+
+  assert "bar" == Redis.current.get("foo")
+
+  Redis.current = Redis.new(OPTIONS.merge(:db => 14))
+
+  assert Redis.current.get("foo").nil?
+end
