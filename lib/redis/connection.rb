cb6a6b927fe529d07d5aa3adc6815865cc2e9e3d
Pieter Noordhuis <pcnoordhuis@gmail.com>
2011-03-18 23:48:12 +0100

    Skip pure Ruby connection when Hiredis is loaded

--- lib/redis/connection.rb
+++ lib/redis/connection.rb
@@ -1,2 +1,4 @@
-require "redis/connection/ruby"
+# Not requiring the pure Ruby connection class when ::Hiredis is defined saves
+# about ~100KB RSS on MRI 1.8.7 and ~150KB RSS on MRI 1.9.2.
+require "redis/connection/ruby" unless defined?(::Hiredis)
 require "redis/connection/hiredis"
