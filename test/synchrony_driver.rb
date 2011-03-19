1de5df6a8d60066c101ed2dc0e8933333080e08f
Ilya Grigorik <ilya@igvita.com>
2011-03-19 12:49:44 -0600

    test pipelining with async driver

--- test/synchrony_driver.rb
+++ test/synchrony_driver.rb
@@ -2,13 +2,14 @@
 
 require 'eventmachine'
 require 'em-synchrony'
-require 'hiredis'
 
 require File.expand_path("./helper", File.dirname(__FILE__))
 
-setup do
-  init Redis.new(OPTIONS)
-end
+#
+# if running under Eventmachine + Synchrony (Ruby 1.9+), then
+# we can simulate the blocking API while performing the network
+# IO via the EM reactor.
+#
 
 EM.synchrony do
   r = Redis.new
@@ -19,6 +20,33 @@ EM.synchrony do
 
   assert 2 == r.llen("foo")
   assert "s2" == r.rpop("foo")
+
+  r.set("foo", "bar")
+
+  assert "bar" == r.getset("foo", "baz")
+  assert "baz" == r.get("foo")
+
+  r.set("foo", "a")
+
+  assert_equal 1, r.getbit("foo", 1)
+  assert_equal 1, r.getbit("foo", 2)
+  assert_equal 0, r.getbit("foo", 3)
+  assert_equal 0, r.getbit("foo", 4)
+  assert_equal 0, r.getbit("foo", 5)
+  assert_equal 0, r.getbit("foo", 6)
+  assert_equal 1, r.getbit("foo", 7)
+
+  r.flushdb
+
+  # command pipelining
+  r.pipelined do
+    r.lpush "foo", "s1"
+    r.lpush "foo", "s2"
+  end
+
+  assert 2 == r.llen("foo")
+  assert "s2" == r.lpop("foo")
+  assert "s1" == r.lpop("foo")
 
   EM.stop
 end
