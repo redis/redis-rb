a8b4a874f2d177ea571ff5dfc0902e2c55bb68af
Ilya Grigorik <ilya@igvita.com>
2011-03-19 12:48:57 -0600

    cleanup async driver, add unix logic, handle connect fail case

--- lib/redis/connection/synchrony.rb
+++ lib/redis/connection/synchrony.rb
@@ -1,62 +1,50 @@
-require "eventmachine"
-require "em-synchrony"
 require "hiredis/reader"
 
 class Redis
   module Connection
+    class RedisClient < EventMachine::Connection
+      include EventMachine::Deferrable
 
-    class RedisClient < EM::Connection
-      include EM::Deferrable
+      def post_init
+        @req = nil
+        @reader = ::Hiredis::Reader.new
+      end
 
       def connection_completed
-        p [:connection_completed]
-
-        @in = nil
-        @messages = []
-        @reader = ::Hiredis::Reader.new
-
         succeed
       end
 
       def receive_data(data)
-        p [:got, data]
-
         @reader.feed(data)
         until (reply = @reader.gets) == false
-          @messages << reply
-          @in.succeed @messages.shift if @in
+          @req.succeed reply
         end
-
       end
 
       def read
-        msg = @messages.shift || sync
+        @req = EventMachine::DefaultDeferrable.new
+        EventMachine::Synchrony.sync @req
       end
 
       def send(data)
-        p [:send, data]
-        callback { p [:sending, data]; send_data data }
+        callback { send_data data }
       end
 
       def unbind
-        p [:unbind]
-        @messages.clear
-        @in.fail if @in
-        @in = nil
-      end
-
-      private
-
-        def sync
-          @in = EventMachine::DefaultDeferrable.new
-          EventMachine::Synchrony.sync @in
+        if @req
+          @req.fail
+          @req = nil
+        else
+          fail
         end
-
+      end
     end
 
     class Synchrony
+      include Redis::Connection::CommandHelper
+
       def initialize
-        @timeout = 5
+        @timeout = 5_000_000
         @state = :disconnected
         @connection = nil
       end
@@ -70,57 +58,26 @@ class Redis
       end
 
       def connect(host, port, timeout)
-        p [:connect, host, port, timeout]
-        f = Fiber.current
         conn = EventMachine.connect(host, port, RedisClient) do |c|
-          # c.connection_timeout = timeout
+          c.pending_connect_timeout = Integer(timeout / 1_000_000)
         end
 
-        conn.callback do
-          @connection = conn
-          @state = :connected
-          f.resume conn
-        end
-
-        conn.errback do
-          @connection = conn
-          f.resume conn
-        end
-
-        Fiber.yield
+        setup_connect_callbacks(conn, Fiber.current)
       end
 
-      # def connect_unix(path, timeout)
-      #   @connection.connect_unix(path, timeout)
-      # rescue Errno::ETIMEDOUT
-      #   raise Timeout::Error
-      # end
+      def connect_unix(path, timeout)
+        conn = EventMachine.connect_unix_domain(path, RedisClient)
+        setup_connect_callbacks(conn, Fiber.current)
+      end
 
       def disconnect
         @state = :disconnected
-        # @connection.close_after_writing
+        @connection.close_connection
         @connection = nil
       end
 
-      COMMAND_DELIMITER = "\r\n"
-
       def write(command)
         @connection.send(build_command(*command).join(COMMAND_DELIMITER))
-      end
-
-      def build_command(*args)
-        command = []
-        command << "*#{args.size}"
-
-        args.each do |arg|
-          arg = arg.to_s
-          command << "$#{string_size arg}"
-          command << arg
-        end
-
-        # Trailing delimiter
-        command << ""
-        command
       end
 
       def read
@@ -129,26 +86,25 @@ class Redis
         raise ::Redis::ProtocolError.new(err.message)
       end
 
-      protected
-      if "".respond_to?(:bytesize)
-        def string_size(string)
-          string.to_s.bytesize
-        end
-      else
-        def string_size(string)
-          string.to_s.size
-        end
-      end
+      private
 
-      if defined?(Encoding::default_external)
-        def encode(string)
-          string.force_encoding(Encoding::default_external)
+        def setup_connect_callbacks(conn, f)
+          conn.callback do
+            @connection = conn
+            @state = :connected
+            f.resume conn
+          end
+
+          conn.errback do
+            @connection = conn
+            f.resume :refused
+          end
+
+          r = Fiber.yield
+          raise Errno::ECONNREFUSED if r == :refused
+          r
         end
-      else
-        def encode(string)
-          string
-        end
-      end
+
     end
   end
 end
