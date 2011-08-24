76f9640c59137acca4dab568b1ce181372952975
Ivan Povalyukhin <ipoval@ya.ru>
2011-08-23 23:49:21 -0700

    Redis::Distributed::CannotDistribute becomes Redis::CannotDistribute and inherites from Redis::Error

--- test/distributed_transactions_test.rb
+++ test/distributed_transactions_test.rb
@@ -11,23 +11,23 @@ end
 test "MULTI/DISCARD" do |r|
   @foo = nil
 
-  assert_raise Redis::Distributed::CannotDistribute do
+  assert_raise Redis::CannotDistribute do
     r.multi { @foo = 1 }
   end
 
   assert nil == @foo
 
-  assert_raise Redis::Distributed::CannotDistribute do
+  assert_raise Redis::CannotDistribute do
     r.discard
   end
 end
 
 test "WATCH/UNWATCH" do |r|
-  assert_raise Redis::Distributed::CannotDistribute do
+  assert_raise Redis::CannotDistribute do
     r.watch("foo")
   end
 
-  assert_raise Redis::Distributed::CannotDistribute do
+  assert_raise Redis::CannotDistribute do
     r.unwatch
   end
 end
