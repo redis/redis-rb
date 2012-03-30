0d3eb6da812322b2e9f5f9ed6403fec0a6f597fb
Luca Guidi <me@lucaguidi.com>
2012-03-30 10:22:23 +0200

    Enabled again failing test

--- test/scripting_test.rb
+++ test/scripting_test.rb
@@ -50,5 +50,5 @@ end
 
 test "SCRIPT EXISTS with existing script" do |r|
   r.script_load("return 23")
-  # assert r.script_exists("c2164f952111fa72ceade53d02f21b514b899fac")
+  assert r.script_exists("c2164f952111fa72ceade53d02f21b514b899fac")
 end
