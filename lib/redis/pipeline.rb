4d3cbae6918a24d6c039ecfde3a4c15a48aebf6c
Pieter Noordhuis <pcnoordhuis@gmail.com>
2012-05-09 12:30:31 -0700

    Transform RuntimeError to CommandError

--- lib/redis/pipeline.rb
+++ lib/redis/pipeline.rb
@@ -46,9 +46,15 @@ class Redis
       yield
     end
 
-    def finish(replies)
-      futures.each_with_index.map do |future, i|
-        future._set(replies[i])
+    def finish(replies, &blk)
+      if blk
+        futures.each_with_index.map do |future, i|
+          future._set(blk.call(replies[i]))
+        end
+      else
+        futures.each_with_index.map do |future, i|
+          future._set(replies[i])
+        end
       end
     end
 
@@ -61,7 +67,12 @@ class Redis
           raise replies.detect { |r| r.kind_of?(::RuntimeError) }
         end
 
-        super(replies.last)
+        super(replies.last) do |reply|
+          # Because an EXEC returns nested replies, hiredis won't be able to
+          # convert an error reply to a CommandError instance itself. This is
+          # specific to MULTI/EXEC, so we solve this here.
+          reply.is_a?(::RuntimeError) ? CommandError.new(reply.message) : reply
+        end
       end
 
       def commands
