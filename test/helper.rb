8707e8b0acefe8e0803003b412d99c0c4caa88d3
Pieter Noordhuis <pcnoordhuis@gmail.com>
2012-05-09 13:53:44 -0700

    Move per-driver code to separate files

--- test/helper.rb
+++ test/helper.rb
@@ -75,61 +75,16 @@ end
 
 $VERBOSE = true
 
-require "redis/connection/%s" % (ENV["conn"] || "ruby")
+ENV["conn"] ||= "ruby"
+
+require "redis/connection/#{ENV["conn"]}"
 require "redis"
 require "redis/distributed"
 
+require "support/connection/#{ENV["conn"]}"
+
 def driver
   Redis::Connection.drivers.last.to_s.split("::").last.downcase.to_sym
-end
-
-if driver == :synchrony
-  module Helper
-    def around
-      rv = nil
-
-      EM.synchrony do
-        begin
-          rv = yield
-        ensure
-          EM.stop
-        end
-      end
-
-      rv
-    end
-  end
-
-  class Wire < Fiber
-    # We cannot run this fiber explicitly because EM schedules it. Resuming the
-    # current fiber on the next tick to let the reactor do work.
-    def self.pass
-      f = Fiber.current
-      EM.next_tick { f.resume }
-      Fiber.yield
-    end
-
-    def self.sleep(sec)
-      EM::Synchrony.sleep(sec)
-    end
-
-    def initialize(&blk)
-      super
-
-      # Schedule run in next tick
-      EM.next_tick { resume }
-    end
-
-    def join
-      self.class.pass while alive?
-    end
-  end
-else
-  class Wire < Thread
-    def self.sleep(sec)
-      Kernel.sleep(sec)
-    end
-  end
 end
 
 def capture_stderr
