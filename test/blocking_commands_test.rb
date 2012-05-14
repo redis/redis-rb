86b133f90c198b3b1742e0d8c5691dfbebb93590
Pieter Noordhuis <pcnoordhuis@gmail.com>
2012-05-14 15:51:05 -0700

    Some blocking command tests can be reused for Redis::Distributed

--- test/blocking_commands_test.rb
+++ test/blocking_commands_test.rb
@@ -1,49 +1,12 @@
 # encoding: UTF-8
 
 require "helper"
+require "lint/blocking_commands"
 
 class TestBlockingCommands < Test::Unit::TestCase
 
   include Helper
-
-  def setup
-    super
-
-    r.rpush("foo", "s1")
-    r.rpush("foo", "s2")
-    r.rpush("bar", "s1")
-    r.rpush("bar", "s2")
-  end
-
-  def to_protocol(obj)
-    case obj
-    when String
-      "$#{obj.length}\r\n#{obj}\r\n"
-    when Array
-      "*#{obj.length}\r\n" + obj.map { |e| to_protocol(e) }.join
-    else
-      fail
-    end
-  end
-
-  def mock(options = {}, &blk)
-    commands = {
-      :blpop => lambda do |*args|
-        sleep options[:delay] if options.has_key?(:delay)
-        to_protocol([args.first, args.last])
-      end,
-      :brpop => lambda do |*args|
-        sleep options[:delay] if options.has_key?(:delay)
-        to_protocol([args.first, args.last])
-      end,
-      :brpoplpush => lambda do |*args|
-        sleep options[:delay] if options.has_key?(:delay)
-        to_protocol(args.last)
-      end,
-    }
-
-    redis_mock(commands, &blk)
-  end
+  include Lint::BlockingCommands
 
   def assert_takes_longer_than_client_timeout
     timeout = OPTIONS[:timeout]
@@ -59,51 +22,9 @@ class TestBlockingCommands < Test::Unit::TestCase
     end
   end
 
-  def test_blpop
-    assert_equal ["foo", "s1"], r.blpop("foo")
-    assert_equal ["foo", "s2"], r.blpop(["foo"])
-    assert_equal ["bar", "s1"], r.blpop(["bar", "foo"])
-    assert_equal ["bar", "s2"], r.blpop(["foo", "bar"])
-  end
-
-  def test_blpop_timeout
-    mock do |r|
-      assert_equal ["foo", "0"], r.blpop("foo")
-      assert_equal ["foo", "1"], r.blpop("foo", :timeout => 1)
-    end
-  end
-
   def test_blpop_disable_client_timeout
     assert_takes_longer_than_client_timeout do |r|
       assert_equal ["foo", "0"], r.blpop("foo")
-    end
-  end
-
-  def test_blpop_with_old_prototype
-    assert_equal ["foo", "s1"], r.blpop("foo", 0)
-    assert_equal ["foo", "s2"], r.blpop("foo", 0)
-    assert_equal ["bar", "s1"], r.blpop("bar", "foo", 0)
-    assert_equal ["bar", "s2"], r.blpop("foo", "bar", 0)
-  end
-
-  def test_blpop_timeout_with_old_prototype
-    mock do |r|
-      assert_equal ["foo", "0"], r.blpop("foo", 0)
-      assert_equal ["foo", "1"], r.blpop("foo", 1)
-    end
-  end
-
-  def test_brpop
-    assert_equal ["foo", "s2"], r.brpop("foo")
-    assert_equal ["foo", "s1"], r.brpop(["foo"])
-    assert_equal ["bar", "s2"], r.brpop(["bar", "foo"])
-    assert_equal ["bar", "s1"], r.brpop(["foo", "bar"])
-  end
-
-  def test_brpop_timeout
-    mock do |r|
-      assert_equal ["foo", "0"], r.brpop("foo")
-      assert_equal ["foo", "1"], r.brpop("foo", :timeout => 1)
     end
   end
 
@@ -113,47 +34,9 @@ class TestBlockingCommands < Test::Unit::TestCase
     end
   end
 
-  def test_brpop_with_old_prototype
-    assert_equal ["foo", "s2"], r.brpop("foo", 0)
-    assert_equal ["foo", "s1"], r.brpop("foo", 0)
-    assert_equal ["bar", "s2"], r.brpop("bar", "foo", 0)
-    assert_equal ["bar", "s1"], r.brpop("foo", "bar", 0)
-  end
-
-  def test_brpop_timeout_with_old_prototype
-    mock do |r|
-      assert_equal ["foo", "0"], r.brpop("foo", 0)
-      assert_equal ["foo", "1"], r.brpop("foo", 1)
-    end
-  end
-
-  def test_brpoplpush
-    assert_equal "s2", r.brpoplpush("foo", "zap")
-    assert_equal ["s2"], r.lrange("zap", 0, -1)
-  end
-
-  def test_brpoplpush_timeout
-    mock do |r|
-      assert_equal "0", r.brpoplpush("foo", "bar")
-      assert_equal "1", r.brpoplpush("foo", "bar", :timeout => 1)
-    end
-  end
-
   def test_brpoplpush_disable_client_timeout
     assert_takes_longer_than_client_timeout do |r|
       assert_equal "0", r.brpoplpush("foo", "bar")
-    end
-  end
-
-  def test_brpoplpush_with_old_prototype
-    assert_equal "s2", r.brpoplpush("foo", "zap", 0)
-    assert_equal ["s2"], r.lrange("zap", 0, -1)
-  end
-
-  def test_brpoplpush_timeout_with_old_prototype
-    mock do |r|
-      assert_equal "0", r.brpoplpush("foo", "bar", 0)
-      assert_equal "1", r.brpoplpush("foo", "bar", 1)
     end
   end
 end
