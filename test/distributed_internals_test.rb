de769030788ad3b17cc44909ab2fb966c27ed4a5
Spencer Markowski <spencer@theablefew.com>
2012-07-11 13:26:26 -0400

    Update test_override_id to verify Redis::Distributed reports correct id

--- test/distributed_internals_test.rb
+++ test/distributed_internals_test.rb
@@ -25,10 +25,17 @@ class TestDistributedInternals < Test::Unit::TestCase
     assert_equal ["redis://localhost:#{PORT}/15","redis://localhost:#{PORT.next}/15"], redis.nodes.map{|node| node.client.id}
   end
 
+  def test_as_mix_and_match
+    nodes = ["redis://localhost:7389/15",OPTIONS.merge(:host => 'localhost'), OPTIONS.merge( :host => 'localhost', :port => PORT.next) ]
+    redis = Redis::Distributed.new nodes
+    assert_equal ["redis://localhost:7389/15","redis://localhost:#{PORT}/15","redis://localhost:#{PORT.next}/15"], redis.nodes.map{|node| node.client.id}
+  end
+
   def test_override_id
     nodes = [OPTIONS.merge(:host => 'localhost', :id => "test"), OPTIONS.merge( :host => 'localhost', :port => PORT.next, :id => "test1") ]
     redis = Redis::Distributed.new nodes
     assert_equal redis.nodes.first.client.id, "test"
     assert_equal redis.nodes.last.client.id,  "test1"
+    assert_equal "#<Redis client v#{Redis::VERSION} for #{redis.nodes.map(&:id).join(', ')}>", redis.inspect
   end
 end
