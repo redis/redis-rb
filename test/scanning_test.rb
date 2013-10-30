461dd435a7aa3f3b0077f481c5d4219913d6705c
Jan-Erik Rediger <badboy@archlinux.us>
2013-10-30 21:37:55 +0100

    Implement SCAN/HSCAN/SSCAN/ZSCAN
    These commands are now in redis unstable and the upcoming 2.8
    
    Tests are the Tcl tests translated into Ruby.

---
+++ test/scanning_test.rb
@@ -0,0 +1,140 @@
+# encoding: UTF-8
+
+require "helper"
+
+class TestScanning < Test::Unit::TestCase
+
+  include Helper::Client
+
+  def test_scan_basic
+    r.debug :populate, 1000
+
+    cursor = 0
+    all_keys   = []
+    loop {
+      cursor, keys = r.scan cursor
+      all_keys += keys
+      break if cursor == "0"
+    }
+
+    assert_equal 1000, all_keys.uniq.size
+  end
+
+  def test_scan_count
+    r.debug :populate, 1000
+
+    cursor = 0
+    all_keys   = []
+    loop {
+      cursor, keys = r.scan cursor, count: 5
+      all_keys += keys
+      break if cursor == "0"
+    }
+
+    assert_equal 1000, all_keys.uniq.size
+  end
+
+  def test_scan_match
+    r.debug :populate, 1000
+
+    cursor = 0
+    all_keys   = []
+    loop {
+      cursor, keys = r.scan cursor, match: "key:1??"
+      all_keys += keys
+      break if cursor == "0"
+    }
+
+    assert_equal 100, all_keys.uniq.size
+  end
+
+  def test_sscan_with_encoding
+    [:intset, :hashtable].each do |enc|
+      r.del "set"
+
+      prefix = ""
+      prefix = "ele:" if enc == :hashtable
+
+      elements = []
+      100.times { |j| elements << "#{prefix}#{j}" }
+
+      r.sadd "set", elements
+
+      assert_equal enc.to_s, r.object("encoding", "set")
+
+      cursor = 0
+      all_keys   = []
+      loop {
+        cursor, keys = r.sscan "set", cursor
+        all_keys += keys
+        break if cursor == "0"
+      }
+
+      assert_equal 100, all_keys.uniq.size
+    end
+  end
+
+  def test_hscan_with_encoding
+    [:ziplist, :hashtable].each do |enc|
+      r.del "set"
+
+      count = 1000
+      count = 30 if enc == :ziplist
+
+      elements = []
+      count.times { |j| elements << "key:#{j}" << j.to_s }
+
+      r.hmset "hash", *elements
+
+      assert_equal enc.to_s, r.object("encoding", "hash")
+
+      cursor = 0
+      all_keys   = []
+      loop {
+        cursor, keys = r.hscan "hash", cursor
+        all_keys += keys
+        break if cursor == "0"
+      }
+
+      keys2 = []
+      all_keys.each_slice(2) do |k, v|
+        assert_equal "key:#{v}", k
+        keys2 << k
+      end
+
+      assert_equal count, keys2.uniq.size
+    end
+  end
+
+  def test_zscan_with_encoding
+    [:ziplist, :skiplist].each do |enc|
+      r.del "zset"
+
+      count = 1000
+      count = 30 if enc == :ziplist
+
+      elements = []
+      count.times { |j| elements << j << "key:#{j}" }
+
+      r.zadd "zset", elements
+
+      assert_equal enc.to_s, r.object("encoding", "zset")
+
+      cursor = 0
+      all_keys   = []
+      loop {
+        cursor, keys = r.zscan "zset", cursor
+        all_keys += keys
+        break if cursor == "0"
+      }
+
+      keys2 = []
+      all_keys.each_slice(2) do |k, v|
+        assert_equal "key:#{v}", k
+        keys2 << k
+      end
+
+      assert_equal count, keys2.uniq.size
+    end
+  end
+end
