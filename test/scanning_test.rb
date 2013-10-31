aa81254ab2b6bcd75cc62abc816e4ed2d5d0dc34
Joshua Flanagan <joshuaflanagan@gmail.com>
2013-10-31 17:41:35 -0500

    scan_each commands to wrap *SCAN cursor-based calls
    The *scan_each commands can all take a block, and will yield each value
    to the block.
    If no block is provided, an Enumerator is returned.

--- test/scanning_test.rb
+++ test/scanning_test.rb
@@ -54,6 +54,65 @@ class TestScanning < Test::Unit::TestCase
     end
   end
 
+  def test_scan_each_enumerator
+    target_version "2.7.105" do
+
+      r.debug :populate, 1000
+
+      scan_enumerator = r.scan_each
+      assert_equal true, scan_enumerator.is_a?(::Enumerator)
+
+      keys_from_scan = scan_enumerator.to_a.uniq
+      all_keys = r.keys "*"
+
+      assert_equal all_keys.sort, keys_from_scan.sort
+    end
+  end
+
+  def test_scan_each_enumerator_match
+    target_version "2.7.105" do
+
+      r.debug :populate, 1000
+
+      keys_from_scan = r.scan_each(:match => "key:1??").to_a.uniq
+      all_keys = r.keys "key:1??"
+
+      assert_equal all_keys.sort, keys_from_scan.sort
+    end
+  end
+
+  def test_scan_each_block
+    target_version "2.7.105" do
+
+      r.debug :populate, 100
+
+      keys_from_scan   = []
+      r.scan_each {|key|
+        keys_from_scan << key
+      }
+
+      all_keys = r.keys "*"
+
+      assert_equal all_keys.sort, keys_from_scan.uniq.sort
+    end
+  end
+
+  def test_scan_each_block_match
+    target_version "2.7.105" do
+
+      r.debug :populate, 100
+
+      keys_from_scan   = []
+      r.scan_each(:match => "key:1?") {|key|
+        keys_from_scan << key
+      }
+
+      all_keys = r.keys "key:1?"
+
+      assert_equal all_keys.sort, keys_from_scan.uniq.sort
+    end
+  end
+
   def test_sscan_with_encoding
     target_version "2.7.105" do
       [:intset, :hashtable].each do |enc|
@@ -79,6 +138,70 @@ class TestScanning < Test::Unit::TestCase
 
         assert_equal 100, all_keys.uniq.size
       end
+    end
+  end
+
+  def test_sscan_each_enumerator
+    target_version "2.7.105" do
+      elements = []
+      100.times { |j| elements << "ele:#{j}" }
+      r.sadd "set", elements
+
+      scan_enumerator = r.sscan_each("set")
+      assert_equal true, scan_enumerator.is_a?(::Enumerator)
+
+      keys_from_scan = scan_enumerator.to_a.uniq
+      all_keys = r.smembers("set")
+
+      assert_equal all_keys.sort, keys_from_scan.sort
+    end
+  end
+
+  def test_sscan_each_enumerator_match
+    target_version "2.7.105" do
+      elements = []
+      100.times { |j| elements << "ele:#{j}" }
+      r.sadd "set", elements
+
+      keys_from_scan = r.sscan_each("set", :match => "ele:1?").to_a.uniq
+
+      all_keys = r.smembers("set").grep(/^ele:1.$/)
+
+      assert_equal all_keys.sort, keys_from_scan.sort
+    end
+  end
+
+  def test_sscan_each_enumerator_block
+    target_version "2.7.105" do
+      elements = []
+      100.times { |j| elements << "ele:#{j}" }
+      r.sadd "set", elements
+
+      keys_from_scan = []
+      r.sscan_each("set") do |key|
+        keys_from_scan << key
+      end
+
+      all_keys = r.smembers("set")
+
+      assert_equal all_keys.sort, keys_from_scan.uniq.sort
+    end
+  end
+
+  def test_sscan_each_enumerator_block_match
+    target_version "2.7.105" do
+      elements = []
+      100.times { |j| elements << "ele:#{j}" }
+      r.sadd "set", elements
+
+      keys_from_scan = []
+      r.sscan_each("set", :match => "ele:1?") do |key|
+        keys_from_scan << key
+      end
+
+      all_keys = r.smembers("set").grep(/^ele:1.$/)
+
+      assert_equal all_keys.sort, keys_from_scan.uniq.sort
     end
   end
 
@@ -116,6 +239,77 @@ class TestScanning < Test::Unit::TestCase
     end
   end
 
+  def test_hscan_each_enumerator
+    target_version "2.7.105" do
+      count = 1000
+      elements = []
+      count.times { |j| elements << "key:#{j}" << j.to_s }
+      r.hmset "hash", *elements
+
+      scan_enumerator = r.hscan_each("hash")
+      assert_equal true, scan_enumerator.is_a?(::Enumerator)
+
+      keys_from_scan = scan_enumerator.to_a.uniq
+      all_keys = r.hgetall("hash").to_a
+
+      assert_equal all_keys.sort, keys_from_scan.sort
+    end
+  end
+
+  def test_hscan_each_enumerator_match
+    # HSCAN with MATCH is currently crashing:
+    # https://github.com/antirez/redis/issues/1362
+    return
+    target_version "2.7.105" do
+      count = 100
+      elements = []
+      count.times { |j| elements << "key:#{j}" << j.to_s }
+      r.hmset "hash", *elements
+
+      keys_from_scan = r.hscan_each("hash", :match => "key:1?").to_a.uniq
+      all_keys = r.hgetall("hash").to_a.select{|k,v| k =~ /^key:1.$/}
+
+      assert_equal all_keys.sort, keys_from_scan.sort
+    end
+  end
+
+  def test_hscan_each_block
+    target_version "2.7.105" do
+      count = 1000
+      elements = []
+      count.times { |j| elements << "key:#{j}" << j.to_s }
+      r.hmset "hash", *elements
+
+      keys_from_scan = []
+      r.hscan_each("hash") do |*key_value|
+        keys_from_scan << key_value
+      end
+      all_keys = r.hgetall("hash").to_a
+
+      assert_equal all_keys.sort, keys_from_scan.uniq.sort
+    end
+  end
+
+  def test_hscan_each_block_match
+    # HSCAN with MATCH is currently crashing:
+    # https://github.com/antirez/redis/issues/1362
+    return
+    target_version "2.7.105" do
+      count = 1000
+      elements = []
+      count.times { |j| elements << "key:#{j}" << j.to_s }
+      r.hmset "hash", *elements
+
+      keys_from_scan = []
+      r.hscan_each("hash", :match => "key:1?") do |*key_value|
+        keys_from_scan << key_value
+      end
+      all_keys = r.hgetall("hash").to_a.select{|k,v| k =~ /^key:1.$/}
+
+      assert_equal all_keys.sort, keys_from_scan.uniq.sort
+    end
+  end
+
   def test_zscan_with_encoding
     target_version "2.7.105" do
       [:ziplist, :skiplist].each do |enc|
@@ -150,4 +344,78 @@ class TestScanning < Test::Unit::TestCase
       end
     end
   end
+
+  def test_zscan_each_enumerator
+    target_version "2.7.105" do
+      count = 1000
+      elements = []
+      count.times { |j| elements << j << "key:#{j}" }
+      r.zadd "zset", elements
+
+      scan_enumerator = r.zscan_each "zset"
+      assert_equal true, scan_enumerator.is_a?(::Enumerator)
+
+      scores_from_scan = scan_enumerator.to_a.uniq
+      member_scores = r.zrange("zset", 0, -1, :with_scores => true)
+
+      assert_equal member_scores.sort, scores_from_scan.sort
+    end
+  end
+
+  def test_zscan_each_enumerator_match
+    # ZSCAN with MATCH is currently crashing:
+    # https://github.com/antirez/redis/issues/1362
+    return
+    target_version "2.7.105" do
+      count = 1000
+      elements = []
+      count.times { |j| elements << j << "key:#{j}" }
+      r.zadd "zset", elements
+
+      scores_from_scan = r.zscan_each("zset", :match => "key:1??").to_a.uniq
+      member_scores = r.zrange("zset", 0, -1, :with_scores => true)
+      filtered_members = member_scores.select{|k,s| k =~ /^key:1..$/}
+
+      assert_equal filtered_members.sort, scores_from_scan.sort
+    end
+  end
+
+  def test_zscan_each_block
+    target_version "2.7.105" do
+      count = 1000
+      elements = []
+      count.times { |j| elements << j << "key:#{j}" }
+      r.zadd "zset", elements
+
+      scores_from_scan = []
+      r.zscan_each("zset") do |*key_score|
+        scores_from_scan << key_score
+      end
+      member_scores = r.zrange("zset", 0, -1, :with_scores => true)
+
+      assert_equal member_scores.sort, scores_from_scan.sort
+    end
+  end
+
+  def test_zscan_each_block_match
+    # ZSCAN with MATCH is currently crashing:
+    # https://github.com/antirez/redis/issues/1362
+    return
+    target_version "2.7.105" do
+      count = 1000
+      elements = []
+      count.times { |j| elements << j << "key:#{j}" }
+      r.zadd "zset", elements
+
+      scores_from_scan = []
+      r.zscan_each("zset", :match => "key:1??") do |*key_score|
+        scores_from_scan << key_score
+      end
+      member_scores = r.zrange("zset", 0, -1, :with_scores => true)
+      filtered_members = member_scores.select{|k,s| k =~ /^key:1..$/}
+
+      assert_equal filtered_members.sort, scores_from_scan.sort
+    end
+  end
+
 end
