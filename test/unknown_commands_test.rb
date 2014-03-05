926cbb1f2928b6b5e0d1df11c7abb5ad3365e78c
Jared Wyatt <j@wyatt.co>
2014-03-04 19:53:57 -0500

    Add tests for "operation not permitted" error messages

--- test/unknown_commands_test.rb
+++ test/unknown_commands_test.rb
@@ -11,4 +11,17 @@ class TestUnknownCommands < Test::Unit::TestCase
       r.not_yet_implemented_command
     end
   end
+
+  def test_multi_with_block_includes_unknown_commands_in_the_error_message
+    begin
+      redis.multi do |multi|
+        multi.unknown1
+        multi.unknown2
+      end
+    rescue => e
+    end
+
+    assert_match(/unknown1/, e.message)
+    assert_match(/unknown2/, e.message)
+  end
 end
