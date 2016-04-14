dad3f4a0a9e660b9c93ea193f78c2b7f7904b51d
Guo Xiang Tan <tgx_world@hotmail.com>
2016-04-14 11:47:58 +0800

    Typo.

--- test/sentinel_command_test.rb
+++ test/sentinel_command_test.rb
@@ -2,7 +2,7 @@
 
 require File.expand_path("helper", File.dirname(__FILE__))
 
-class SentinalCommandsTest < Test::Unit::TestCase
+class SentinelCommandsTest < Test::Unit::TestCase
 
   include Helper::Client
 
