dbb49937ce4acf78306e9bbac4d0e2db9b50cd12
Eric Proulx <eproulx@petalmd.com>
2018-02-14 10:37:39 -0500

    Added existed_count in distributed

--- lib/redis/distributed.rb
+++ lib/redis/distributed.rb
@@ -166,6 +166,14 @@ class Redis
       node_for(key).exists(key)
     end
 
+    # Count existing keys.
+    def exists_count(*args)
+      keys_per_node = args.group_by { |key| node_for(key) }
+      keys_per_node.inject(0) do |sum, (node, keys)|
+        sum + node.exists_count(*keys)
+      end
+    end
+
     # Find all keys matching the given pattern.
     def keys(glob = "*")
       on_each_node(:keys, glob).flatten
