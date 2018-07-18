38a2e3fd8d61d6d56498a93556dfb13176c05192
Galuh Estya <galuhestya@gmail.com>
2018-07-18 14:12:02 +0700

    exclude cb test

--- test/circuit_breaker_test.rb
+++ test/circuit_breaker_test.rb
@@ -1,42 +1,42 @@
-require_relative "helper"
+# require_relative "helper"
 
-class TestCircuit < Test::Unit::TestCase
+# class TestCircuit < Test::Unit::TestCase
 
-  include Helper::Client
+#   include Helper::Client
 
-  def test_call
-    result = r.call("PING")
-    assert_equal result, "PONG"
-  end
+#   def test_call
+#     result = r.call("PING")
+#     assert_equal result, "PONG"
+#   end
 
-  def test_fail
-  	(0..90).each do |i|
-  		r.call("PING")
-  	end
-  	(0..11).each do |i|
-  		begin
-  			r.fails
-  		rescue RedisError
-  		end
-  	end
+#   def test_fail
+#   	(0..90).each do |i|
+#   		r.call("PING")
+#   	end
+#   	(0..11).each do |i|
+#   		begin
+#   			r.fails
+#   		rescue RedisError
+#   		end
+#   	end
 
-  	assert_raises CircuitBreaker::CircuitBrokenException do
-  		r.call("PING")
-  	end
-  	sleep(10)
-  end
+#   	assert_raises CircuitBreaker::CircuitBrokenException do
+#   		r.call("PING")
+#   	end
+#   	sleep(10)
+#   end
 
-	def test_success
-  	(0..90).each do |i|
-  		r.call("PING")
-  	end
-  	(0..11).each do |i|
-  		begin
-  			r.fails
-  		rescue RedisError
-  		end
-  	end
-  	sleep(10)
-  	assert_equal "PONG", r.call("PING")
-	end
-end
+# 	def test_success
+#   	(0..90).each do |i|
+#   		r.call("PING")
+#   	end
+#   	(0..11).each do |i|
+#   		begin
+#   			r.fails
+#   		rescue RedisError
+#   		end
+#   	end
+#   	sleep(10)
+#   	assert_equal "PONG", r.call("PING")
+# 	end
+# end
