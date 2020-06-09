8669555c60dabd983efc5f6855e659af3b1d0435
Jean Boussier <jean.boussier@gmail.com>
2020-06-09 13:34:56 +0200

    Deprecate the synchrony driver

--- lib/redis/connection/synchrony.rb
+++ lib/redis/connection/synchrony.rb
@@ -6,6 +6,11 @@ require_relative "../errors"
 require "em-synchrony"
 require "hiredis/reader"
 
+Kernel.warn(
+  "The redis synchrony driver is deprecated and will be removed in redis-rb 5.0. " \
+  "We're looking for people to maintain it as a separate gem, see https://github.com/redis/redis-rb/issues/915"
+)
+
 class Redis
   module Connection
     class RedisClient < EventMachine::Connection
