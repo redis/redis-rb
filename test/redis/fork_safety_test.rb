389d8b637713346e9052f4a5c2f1e0cb6a4e8add
Jean Boussier <jean.boussier@gmail.com>
2022-09-22 12:23:23 +0200

    Fix automatic disconnection when the process was forked
    Fix: https://github.com/redis/redis-rb/issues/1157
    Ref: https://github.com/redis-rb/redis-client/commit/bec4931c952b6196c76977356238dbaf4f2e1293
    
    Now that the feature was added to redis-client, we can simply delete code to have it back.

--- test/redis/fork_safety_test.rb
+++ test/redis/fork_safety_test.rb
@@ -11,45 +11,15 @@ class TestForkSafety < Minitest::Test
 
   def test_fork_safety
     redis = Redis.new(OPTIONS)
-    redis.set "foo", 1
-
-    child_pid = fork do
-      # InheritedError triggers a reconnect,
-      # so we need to disable reconnects to force
-      # the exception bubble up
-      redis.without_reconnect do
-        redis.set "foo", 2
+    pid = fork do
+      1000.times do
+        assert_equal "OK", redis.set("key", "foo")
       end
-      exit! 0
-    rescue Redis::InheritedError
-      exit! 127
     end
-
-    _, status = Process.wait2(child_pid)
-
-    assert_equal 127, status.exitstatus
-    assert_equal "1", redis.get("foo")
-  end
-
-  def test_fork_safety_with_enabled_inherited_socket
-    redis = Redis.new(OPTIONS.merge(inherit_socket: true))
-    redis.set "foo", 1
-
-    child_pid = fork do
-      # InheritedError triggers a reconnect,
-      # so we need to disable reconnects to force
-      # the exception bubble up
-      redis.without_reconnect do
-        redis.set "foo", 2
-      end
-      exit! 0
-    rescue Redis::InheritedError
-      exit! 127
+    1000.times do
+      assert_equal "PONG", redis.ping
     end
-
-    _, status = Process.wait2(child_pid)
-
-    assert_equal 0, status.exitstatus
-    assert_equal "2", redis.get("foo")
+    _, status = Process.wait2(pid)
+    assert_predicate(status, :success?)
   end
 end
