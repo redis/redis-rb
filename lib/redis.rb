c205a8c7f22a3d387cb3cf9a8816e7a32f748c5c
Jean Boussier <jean.boussier@gmail.com>
2025-07-17 10:16:58 +0200

    Require redis-client earlier
    Fix: https://github.com/redis/redis-rb/pull/1315

--- lib/redis.rb
+++ lib/redis.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "redis-client"
+
 require "monitor"
 require "redis/errors"
 require "redis/commands"
--- lib/redis/client.rb
+++ lib/redis/client.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'redis-client'
-
 class Redis
   class Client < ::RedisClient
     ERROR_MAPPING = {
